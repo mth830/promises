e36053ebbe0430de55566155c7180e6d295779f9
mth830 <mth830@gmail.com>
2025-06-12 05:14:21 -0400

    updated queue so that there is no need for run

--- asyncQueue.js
+++ asyncQueue.js
@@ -1,35 +1,32 @@
 class asyncQueue {
   constructor(concurrentCount = 2) {
     this.concurrentCount = concurrentCount;
-    this.activeFunctionList = [];
+    this.activeCount = 0;
     this.queuedFunctionList = [];
     this.queueIndex = 0;
   }
   insert = (asyncFunc) => {
+    if(this.activeCount<this.concurrentCount){
+      this.activeCount++;
+      const prom = Promise.resolve();
+      prom.then(this.getNextPromise);
+    }
     this.queuedFunctionList.push(asyncFunc);
 
   }
   getNextPromise = () => {
     if (this.queueIndex + 1 > this.queuedFunctionList.length) {
-      return new Promise((res, rej) => { });
+      return new Promise((res, rej) => { this.activeCount--});
     }
     const next = this.queuedFunctionList[this.queueIndex];
     this.queueIndex++;
     return new Promise((res, rej) => next(this.getNextPromise));
   }
-  run = () => {
-    for (let i = 0; i < this.concurrentCount && i < this.queuedFunctionList.length; i++) {
-      let prom = Promise.resolve();
-      prom.then(this.getNextPromise);
-      this.activeFunctionList.push(prom);
-    }
-    return Promise.all(this.activeFunctionList);
-  }
 }
 //TEST
-const aq = new asyncQueue();
-
-aq.insert(async (cb) => {
+const aq = new asyncQueue(2);
+//Comment out these first 2 for uniform  testing
+/*aq.insert(async (cb) => {
   setTimeout(() => {
     console.log(0);
     cb();
@@ -37,12 +34,12 @@ aq.insert(async (cb) => {
 });
 aq.insert(async (cb) => {
   setTimeout(() => {
-    console.log(0);
+    console.log(1);
     cb();
   }, 12000);
-});
+});*/
 
-for (let i = 1; i < 30; i++) {
+for (let i = 1; i <= 30; i++) {
   let func = async (cb) => {
     setTimeout(() => {
       console.log(i);
@@ -50,6 +47,15 @@ for (let i = 1; i < 30; i++) {
     }, 1000);
   }
   aq.insert(func);
-
 }
-aq.run();
+setTimeout(()=>{
+  for (let i = 31; i <= 40; i++) {
+  let func = async (cb) => {
+    setTimeout(() => {
+      console.log(i);
+      cb();
+    }, 1000);
+  }
+  aq.insert(func);
+}
+},25000);
